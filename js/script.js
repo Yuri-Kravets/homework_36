3a94b48edfc178c381073d4dafc63de5b4c5515b
Yuri-Kravets <kravets.yuri.v@gmail.com>
2022-02-07 16:12:58 +0200

    update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const todoList = {
     form: null,
     
     findForm() {
-        const form =document.getElementById(this.formId);
+        const form = document.getElementById(this.formId);
+// находим и записываем в переменную форму
 
         if(form === null || form.nodeName !== 'FORM') {
             throw new Error ('There is no such form on the page');
@@ -16,63 +17,88 @@ const todoList = {
         return form;
     },
     addFormHandler() {
+// вешаем слушатель событий на кнопку
         this.form.addEventListener(
             'submit',
             (event) => this.formHandler(event)
-        );
+        ); 
     },
-
+    deleteAllTasks () {
+        this.form.addEventListener(
+            'onclick',
+            (event) => this.formHandler(event)
+            );
+            console.log('!!!');
+    },
     preFillTodoList() {
+        //console.log('this ' + bind(this));//fix this
         document
             .addEventListener(
                 'DOMContentLoaded',  
             this.preFillHandler.bind(this)
+
             )
     },
     preFillHandler(){
         const data = this.getData();
-        //console.log(data);
-        data.forEach(todoItem => {
-            const template = this.createTemplate(todoItem);
-            document
-                .getElementById('todoItems')
-                .prepend(template);
-        })
-        
+       // console.dir(data);
+        if(data !== null) {
+            if (data.length > 0) {
+                data.forEach(todoItem => {
+                    const template = this.createTemplate(todoItem);
+                    document
+                        .getElementById('todoItems')
+                        .prepend(template);
+                })
+            }
+        }
     },
 
     formHandler(event) {
         event.preventDefault();
-        console.log('form submitted');
+        
+        if (event.submitter.id === 'del'){
+            localStorage.clear();
+            document.getElementById('todoItems')
+                .remove();
+            return
+        }
 
+        
+// отменяем перезагрузку страницы        
+        //console.log('form submitted');
         const inputs = this.findInputs(event.target);
+// записываем в переменную инпуты из формы
         const data = {};
+        //console.log(event.target);
+// записываем в обьект ключ и значение каждого инпута
+            inputs.forEach(input => {
+                data[input.name] = input.value;
+            });
+// вызываем функцию записи данных в localstorage
+            this.setData(data);
+            const template = this.createTemplate(data);
 
-        inputs.forEach(input => {
-            data[input.name] = input.value;
-        });
-
-        this.setData(data);
-        const template = this.createTemplate(data);
-
-        document.getElementById('todoItems')
-            .prepend(template);
-            console.log(data);
-        event.target.reset();
+            document.getElementById('todoItems')
+                .prepend(template);
+                //console.log(event.target);
+                event.target.reset();
+        
         
     },
     setData(data) {
         if(!localStorage.getItem(this.formId)) {
             let arr = [];
             arr.push(data);
-
             localStorage.setItem(
                 this.formId,
                 JSON.stringify(arr)
             );
             return;
         }
+        
         let existingData = localStorage.getItem(this.formId);
+       
         existingData =JSON.parse(existingData);
         existingData.push(data);
         localStorage.setItem(
@@ -93,12 +119,18 @@ const todoList = {
 
     init(todoListFormId) {
         if(typeof todoListFormId !== 'string' || todoListFormId.length === 0)  {
+// проверяем существует ли такой id
             throw new Error ('To do list ID is not valid');
         }
         this.formId = todoListFormId;
+// записываем в переменную id формы
         this.findForm();
+// вызов функции поиска формы
         this.addFormHandler();
+// вызов функции слушателя событий
         this.preFillTodoList();
+        //this.deleteAllTasks ();
+        
     }, 
 
     createTemplate({title,description}) {
@@ -107,9 +139,7 @@ const todoList = {
 
         const taskWrapper = this.createElement('div','taskWrapper');
         todoItem.classList.add('taskWrapper');
-
         todoItem.append(taskWrapper);
-
         const taskHeading = this.createElement(
             'div',
             'taskHeading',
@@ -148,5 +178,10 @@ const todoList = {
 
 
 todoList.init('todoForm');
+// Передаем id формы в функцию init
+
+localStorage.setItem('name','yuri');
+
+localStorage.getItem('name');
 
 })();
